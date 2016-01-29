d8175be3b1c4875df285e35b6ccc1e580f3a51bb
Olex <>
2016-01-29 11:03:59 -0500

    This shit is fucked up

--- src/projectdnimoy/others/Cannon.java
+++ src/projectdnimoy/others/Cannon.java
@@ -1,36 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projectdnimoy.others;
 
+import javafx.animation.RotateTransition;
+import javafx.animation.Transition;
+import javafx.geometry.Insets;
 import javafx.scene.chart.LineChart;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 /**
  *
  * @author Olex Yasinovskyy
  */
 public class Cannon extends AbstractWindow {
-
-    Cannon(){
+    
+    
+    
+    public Cannon(){
         setTitle(TOPICS[MECH_ID][0]);
     }
     
-    double h= 0, d=0, teta=0, v=0, t=0;
-    final double G = 9.80665, MIN_H = 0;
+    double h= 0, d=0, tet=0, v=0;
+    double time = 0; //in abstract
+    final double G = 9.80665, MIN_H = 0, MIN_D =0;
     
-    private double calculateHeight(double t) {
-        this.t = t;
-        h = MIN_H - 0.5*G; // Popraviti formulu, dobaviti Vy*t
+    private double calculateHeight(double tet, double v, double t) {
+       h = MIN_H + Math.sin(Math.toRadians(tet))*v*t - G*0.5*Math.pow(t, 2);
         return h;
     }
     
-    private void calculateDistance() {
-        
+    private double calculateDistance(double tet, double v, double t) {
+        d = MIN_D + v*t*Math.cos(Math.toRadians(tet));
+        return d;
     }
-    
+        
     @Override
     public void pauseAnimations() {
         
@@ -48,7 +53,14 @@ public class Cannon extends AbstractWindow {
 
     @Override
     public Pane initAnimationPane() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       GridPane anim = new GridPane();
+       anim.setHgap(10);
+       anim.setVgap(10);
+       anim.setPadding(new Insets(400));
+       
+       
+       
+       return anim; 
     }
 
     @Override
@@ -56,4 +68,14 @@ public class Cannon extends AbstractWindow {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    public static void main(String[] a){
+       launch(a);
+    }
+
+    private class cannonClass {
+       Image image = new Image("projectDNIMOY/others/cannon.png");
+       StackPane view;
+       ImageView iv;
+       RotateTransition trans = new RotateTransition();
+    }   
 }
