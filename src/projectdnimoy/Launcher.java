1beec007623d79372399161bc07b83482e916bcd
Daniel <>
2016-01-29 11:04:27 -0500

    Positioning nodes in menu

--- src/projectdnimoy/Launcher.java
+++ src/projectdnimoy/Launcher.java
@@ -8,25 +8,28 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
 
 public class Launcher extends Application implements ConstantsInterface {
-    
+    Stage primaryStage;
     @Override
     public void start(Stage primaryStage) {
-        
-        HBox root = new HBox();
+        this.primaryStage = primaryStage;
+        BorderPane root = new BorderPane();
         root.getChildren().add(getTopicPane(MECH_ID));
         root.getChildren().add(getTopicPane(WAVES_ID));
         root.getChildren().add(getTopicPane(ENM_ID));
-        root.setBackground(new Background(new BackgroundImage(new Image("projectdnimoy/Menu.jpg")
+        Image i = new Image("projectdnimoy/Menu.jpg");
+        root.setBackground(new Background(new BackgroundImage(i
                 , BackgroundRepeat.REPEAT,BackgroundRepeat.REPEAT
                 , BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
+        
         // Exit button 
         Scene scene = new Scene(root, width, height);
-        
+        primaryStage.setResizable(false);
         primaryStage.setTitle("Menu");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -38,8 +41,11 @@ public class Launcher extends Application implements ConstantsInterface {
         out.setPadding(new Insets(10));
         out.setPrefWidth(width/4);
         Text title = new Text(SUBJECTS[sub]);
+        title.setFill(Color.WHITE);
+        title.setFont(Font.font(14));
+       
         out.getChildren().add(title);
-        VBox.setMargin(title, new Insets(height/2-100, 10, 10, 10)); //needs more precision
+        //VBox.setMargin(title, new Insets(height/2-100, 10, 10, 10)); //needs more precision
         Button b;
         for(int i : new int[]{0, 1, 2}) {
             b = getTopicButton(sub, i);
@@ -61,11 +67,13 @@ public class Launcher extends Application implements ConstantsInterface {
             b.setOnAction((ActionEvent e)->JOptionPane.showMessageDialog(null, IN_CONS_MESSAGE, IN_CONS_TITLE, JOptionPane.INFORMATION_MESSAGE));
         }
         b.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.MEDIUM)));
-        
+        b.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        b.setTextFill(Color.WHITE);
+        b.setFont(Font.font(14));
         return b;
     }
     
-    private static void launchWindow(int subject, int choice) {
+    private void launchWindow(int subject, int choice) {
         
     }
     
