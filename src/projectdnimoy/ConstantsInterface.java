ed5856233a18d25bb1b79b598ce0d040bdacb030
Daniel <>
2016-02-04 09:47:02 -0500

    Menu almost finished

--- src/projectdnimoy/ConstantsInterface.java
+++ src/projectdnimoy/ConstantsInterface.java
@@ -6,14 +6,16 @@ public interface ConstantsInterface {
     int MECH_ID = 0, WAVES_ID = 1, ENM_ID = 2;
     int width = 800, height = 600;
     
-    String[] SUBJECTS = new String[]{"MECHANICS", "WAVES AND OPTICS", "ElECTRICITY AND MAGNETISM"};
-    String[][] TOPICS = new String[][]{{"2D Ballistics", "Collisions and Momentum"},
-                                        {"Geometrical Optics", "Pendulum Oscillation"},
-                                        {"Forces of Point Charges", "Capacitance and Resistance"}};
+    String[] SUBJECTS = new String[]{" MECHANICS ", "WAVES AND OPTICS ", "ELECTRICITY AND\n  MAGNETISM "};
+    String[][] TOPICS = new String[][]{{"2D Ballistics", "Collisions and\n Momentum"},
+                                        {"Geometrical\n Optics", "Pendulum\n Oscillation"},
+                                        {"Point Charges", "Capacitance and\n Resistance"}};
     Color[] SUBJECT_COLORS = new Color[]{Color.BLUE, Color.YELLOW, Color.RED};
     String IN_CONS_TITLE = "In Construction";
     String IN_CONS_MESSAGE = "Section currently in construction. You will now be returned to the main menu.";
     String HELP_TEXT = "Help", DONE_TEXT = "Done", RESET_TEXT = "Reset", PLAY_TEXT = "Play", PAUSE_TEXT = "Pause";
-    double[] xSubPosition = new double[]{100 , 200 , 300};
-    double[] ySubPosition = new double[]{10, 50 , 80};
+    double[] xSubPosition = new double[]{100 , 320 , 540};
+    double[] ySubPosition = new double[]{10 , 130 , 260};
+    
+   
 }
--- src/projectdnimoy/Launcher.java
+++ src/projectdnimoy/Launcher.java
@@ -3,6 +3,7 @@ package projectdnimoy;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -10,6 +11,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
 
@@ -18,7 +20,7 @@ public class Launcher extends Application implements ConstantsInterface {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        BorderPane root = new BorderPane();
+        Pane root = new Pane();
         root.getChildren().add(getTopicPane(MECH_ID));
         root.getChildren().add(getTopicPane(WAVES_ID));
         root.getChildren().add(getTopicPane(ENM_ID));
@@ -27,7 +29,18 @@ public class Launcher extends Application implements ConstantsInterface {
                 , BackgroundRepeat.REPEAT,BackgroundRepeat.REPEAT
                 , BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
         
-        // Exit button 
+        Button exit = new Button("  EXIT  ");
+        exit.setLayoutX(407);
+        exit.setLayoutY(430);
+        exit.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.NONE, CornerRadii.EMPTY, BorderStroke.DEFAULT_WIDTHS)));
+        exit.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        exit.setTextFill(Color.WHITE);
+        exit.setFont(Font.font(20));
+        exit.setOnAction((ActionEvent event) -> {
+            System.exit(0);
+        }) ;
+        root.getChildren().add(exit);
+        
         Scene scene = new Scene(root, width, height);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Menu");
@@ -38,19 +51,22 @@ public class Launcher extends Application implements ConstantsInterface {
     
     private VBox getTopicPane(int sub) {
         VBox out = new VBox();
-        out.setPadding(new Insets(10));
+        out.setPadding(new Insets(20));
         out.setPrefWidth(width/4);
         Text title = new Text(SUBJECTS[sub]);
         title.setFill(Color.WHITE);
         title.setFont(Font.font(14));
-       
+        out.setAlignment(Pos.CENTER);
+        out.setLayoutX(xSubPosition[sub]);
+        out.setLayoutY(ySubPosition[sub]);
         out.getChildren().add(title);
-        //VBox.setMargin(title, new Insets(height/2-100, 10, 10, 10)); //needs more precision
+        
+        
         Button b;
         for(int i : new int[]{0, 1, 2}) {
             b = getTopicButton(sub, i);
             out.getChildren().add(b);
-            VBox.setMargin(b, new Insets(10));
+            VBox.setMargin(b, new Insets(7));
         }
         
        
@@ -66,8 +82,10 @@ public class Launcher extends Application implements ConstantsInterface {
             b = new Button(IN_CONS_TITLE);
             b.setOnAction((ActionEvent e)->JOptionPane.showMessageDialog(null, IN_CONS_MESSAGE, IN_CONS_TITLE, JOptionPane.INFORMATION_MESSAGE));
         }
-        b.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderStroke.MEDIUM)));
+        b.setBorder(new Border(new BorderStroke(Color.STEELBLUE, BorderStrokeStyle.DASHED, CornerRadii.EMPTY, BorderStroke.DEFAULT_WIDTHS)));
         b.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        b.setTextAlignment(TextAlignment.CENTER);
+        b.setPrefSize(200, 50);
         b.setTextFill(Color.WHITE);
         b.setFont(Font.font(14));
         return b;
